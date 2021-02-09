067d0fe09e62229d8d58b7b745e0bbe7b507dd56
Igor Lisboa <igorlisboa34@gmail.com>
2021-02-09 20:21:05 -0300

    Aula 2 atividade 1

--- app/src/main/java/br/com/igorlisboa/agenda/MainActivity.java
+++ app/src/main/java/br/com/igorlisboa/agenda/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "Hello World!!!!!", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
     }
 }
