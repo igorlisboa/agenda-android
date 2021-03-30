814adfbb7758e479639bee89583ec84f3c893232
Igor Lisboa <igorlisboa34@gmail.com>
2021-03-29 23:00:27 -0300

    Aula 4 atividade 3

---
+++ app/src/main/java/br/com/igorlisboa/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,16 @@
+package br.com.igorlisboa.agenda.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.igorlisboa.agenda.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
--- app/src/main/java/br/com/igorlisboa/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/igorlisboa/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,11 +1,8 @@
-package br.com.igorlisboa.agenda;
+package br.com.igorlisboa.agenda.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -15,18 +12,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import br.com.igorlisboa.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "Hello World!!!!!", Toast.LENGTH_LONG).show();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de alunos");
 
         List<String> alunos = new ArrayList<>(Arrays.asList("João", "Igor", "Fernando", "Pedro", "João", "Igor", "Fernando", "Pedro","João", "Igor", "Fernando", "Pedro","João", "Igor", "Fernando", "Pedro","João", "Igor", "Fernando", "Pedro","João", "Igor", "Fernando", "Pedro"));
 
-        ListView listaAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaAlunos = findViewById(R.id.activity_lista_de_alunos_listview);
         listaAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
