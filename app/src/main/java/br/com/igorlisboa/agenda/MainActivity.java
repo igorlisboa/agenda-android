3fdbe4d8813f68cbfa033026c8d0ded737932526
Igor Lisboa <igorlisboa34@gmail.com>
2021-03-29 22:47:21 -0300

    Aula 4 atividade 2

--- app/src/main/java/br/com/igorlisboa/agenda/MainActivity.java
+++ app/src/main/java/br/com/igorlisboa/agenda/MainActivity.java
@@ -9,18 +9,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "Hello World!!!!!", Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
+        setTitle("Lista de alunos");
 
         List<String> alunos = new ArrayList<>(Arrays.asList("João", "Igor", "Fernando", "Pedro", "João", "Igor", "Fernando", "Pedro","João", "Igor", "Fernando", "Pedro","João", "Igor", "Fernando", "Pedro","João", "Igor", "Fernando", "Pedro","João", "Igor", "Fernando", "Pedro"));
 
