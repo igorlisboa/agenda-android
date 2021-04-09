fd0a79ae3c79a17be0b044ce7771741bc17cda11
Igor Lisboa <igorlisboa34@gmail.com>
2021-04-09 20:11:59 -0300

    Aula 4 atividade 5

--- app/src/main/java/br/com/igorlisboa/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/igorlisboa/agenda/ui/activity/FormularioAlunoActivity.java
@@ -3,6 +3,9 @@ package br.com.igorlisboa.agenda.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import br.com.igorlisboa.agenda.R;
 
@@ -12,5 +15,14 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioAlunoActivity.this,"Cliquei no salvar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
